1138ea79c0ed6b1b50f346af45173cf76a8db2c8
Lisak <liska2@spsejecna.cz>
2024-06-05 01:41:20 +0200

    Preparing everything for the API elements

--- WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/MainWindow.xaml.cs
@@ -1,22 +1,69 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Windows.Threading;
 
 namespace WeatherApp
 {
 
-
-    public partial class MainWindow : Window
+    
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        // API key
+        private readonly string apiKey = "465b213001cd4373a3c170538240406";
+        private string cityName;
+
+        // Making a real time clock
+        private DateTime _now;
+        public DateTime Now
+        {
+            get { return _now; }
+            set
+            {
+                _now = value;
+                OnPropertyChanged(nameof(Now));
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            timer.Tick += (sender, args) =>
+            {
+                Now = DateTime.Now;
+            };
+            timer.Start();
         }
 
-        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(String propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
 
         }
+        // Search box funtcion
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
 
-        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        }
+        
+        // Power off button
+        private void PowerOffBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            
+        }
+        // Button for searching citties
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
         {
 
         }
